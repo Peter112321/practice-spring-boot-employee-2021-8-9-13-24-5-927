0fb603fca4525bd1e077cc0da56db828798ef924
Peter <peter.li@agileexlab.net>
2021-10-31 18:47:40 +0800

    [Restfulapi][Peter]:test: passed service test 4

--- src/test/java/com/afs/restfulapi/CompanyServiceTest.java
+++ src/test/java/com/afs/restfulapi/CompanyServiceTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -42,7 +47,7 @@ public class CompanyServiceTest {
 
     //2
     @Test
-    void should_return_Company_when_get_Company__CompanyId() {
+    void should_return_Company_when_get_Company_given_CompanyId() {
         //given
         List<Company> companies = Arrays.asList(
                 new Company("a"),
@@ -55,5 +60,24 @@ public class CompanyServiceTest {
        Company actual= companyService.getCompanyById(1);
         //then
         assertEquals(actual,companies.get(0));
+    }
+    //3
+
+    //4
+    @Test
+    void should_return_page_when_get_Company_given_page_and_pageSize() {
+        //given
+        List<Company> companies = Arrays.asList(
+                new Company("a"),
+                new Company("b"),
+                new Company("c")
+        );
+        List<Company> expected=companies.stream().skip(1).limit(1).collect(Collectors.toList());
+        when(companyRepository.findAll(any(Pageable.class)))
+                .thenReturn(new PageImpl(expected));
+        //when
+        Page<Company> actual= companyService.getCompanyListByPage(1,1);
+        //then
+        assertEquals(actual,new PageImpl(expected));
     }
 }

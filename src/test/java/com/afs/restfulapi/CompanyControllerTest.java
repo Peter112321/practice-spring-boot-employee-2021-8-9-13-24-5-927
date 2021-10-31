9229eaebbafe4c59f3c2194d77eb73712ff43922
Peter <peter.li@agileexlab.net>
2021-10-31 17:14:50 +0800

    [Restfulapi][Peter]:test: passed test 2/3

--- src/test/java/com/afs/restfulapi/CompanyControllerTest.java
+++ src/test/java/com/afs/restfulapi/CompanyControllerTest.java
@@ -1,7 +1,6 @@
 package com.afs.restfulapi;
 
 import com.afs.restfulapi.Entity.Company;
-import com.afs.restfulapi.Entity.Employee;
 import com.afs.restfulapi.Repository.CompanyRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +9,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @Sql(statements = "alter table employee alter column id restart with 1")
@@ -33,7 +31,7 @@ public class CompanyControllerTest {
     }
 
     @Test
-    void Should_return_all_companies_when_get_companies_given_two_companies() throws Exception{
+    void Should_return_all_companies_when_get_companies_given_two_companies() throws Exception {
         //given
         Company a = new Company("a");
         Company b = new Company("b");
@@ -53,12 +51,45 @@ public class CompanyControllerTest {
 
     }
 
+    @Test
+    void Should_return_Specific_companies_when_get_companies_given_company_id() throws Exception {
+        //given
+        Company a = new Company("a");
+        Company b = new Company("b");
+        companyRepository.save(a);
+        companyRepository.save(b);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/companies/2"));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(b.getCompanyId()))
+                .andExpect(jsonPath("$.name").value(b.getCompanyName()));
+
+    }
+
+    @Test
+    void Should_return_company_page_when_get_companies_given_page_size_and_page_number() throws Exception {
+        //given
+        Company a = new Company("a");
+        Company b = new Company("b");
+        Company c = new Company("c");
+
+        companyRepository.save(a);
+        companyRepository.save(b);
+        companyRepository.save(c);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/companies?pageSize=1?page=1"));
+
+        //when
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[1].id").value(b.getCompanyId()))
+                .andExpect(jsonPath("$[1].name").value(b.getCompanyName()));
 
 
-
-
-
-
-
-
+    }
 }
